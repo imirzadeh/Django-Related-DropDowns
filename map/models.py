c68d587d2ea95f0c8e37a78937f6bb9c492a5f4d
Hyperion <lasagnes.tomate@gmail.com>
2020-06-18 19:18:49 +0200

    Value in admin interface: Overwritten __str__ function to print object name in UI
    Signed-off-by: Hyperion <lasagnes.tomate@gmail.com>

--- map/models.py
+++ map/models.py
@@ -5,12 +5,18 @@ class City(models.Model):
     name = models.CharField(max_length=50)
     country = models.ForeignKey("Country", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
     def __unicode__(self):
         return u'%s' % (self.name)
 
 
 class Country(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
     def __unicode__(self):
         return u'%s' % (self.name)
